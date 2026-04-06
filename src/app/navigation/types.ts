6788c71649087fa74201352aa7d7178f88605c1a
Tonysw2 <anthonyrmataa@gmail.com>
2026-04-06 12:41:53 -0300

    chore: remove onboarding types from global RootParamList

--- src/app/navigation/types.ts
+++ src/app/navigation/types.ts
@@ -1,10 +1,7 @@
 import type { AuthStackParamList } from './auth-stack/types'
-import type { OnboardingStackParamList } from '@ui/screens/onboarding/onboarding-stack/types'
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList
-      extends AuthStackParamList,
-        OnboardingStackParamList {}
+    interface RootParamList extends AuthStackParamList {}
   }
 }
